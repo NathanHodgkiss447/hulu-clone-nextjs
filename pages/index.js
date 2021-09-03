6eff71a7eb76b5678203763e497d611f897c2bbe
NathanHodgkiss447 <nathanhodgkiss447@gmail.com>
2021-09-03 14:56:09 +0100

    import header

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,23 @@
 import Head from "next/head";
 import Image from "next/image";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div className="">
       <Head>
         <title>Hulu Clone</title>
-        <meta name="description" content="This is a clone of Hulu created using NextJS and Tailwind" />
+        <meta
+          name="description"
+          content="This is a clone of Hulu created using NextJS and Tailwind"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <h1>Hello World</h1>
 
       {/* Header */}
-
+      <Header />
       {/* Navigation */}
 
       {/* Results */}
